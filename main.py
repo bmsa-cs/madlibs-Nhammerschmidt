f20fe70147080ed7bd6853d580fc3d89f6bc0f07
Natalie Hammerschmidt <nhammerschmidt24@biomedscienceacademy.org>
2020-11-30 18:11:39 +0000

    done

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Natalie Hammerschmidt 
+Period/Core:7
 
 
 """
+
+baseballteam_1 = input("Enter you favorite Baseball team.")
+baseballteam_2 = input("Enter your second favorite baseball team.")
+favoritefood = input("Enter your favorite stadium food.")
+icecreamflavor_1 = input("Enter your favorite ice cream flavor.")
+color_1 = input("Enter any color.")
+name_1 = input("Enter any name")
+verb_1 = input("Enter a verb that is the opposite of walked")
+maskcots_1 = input("What is the {baseballteam_1} maskcot?")
+batting_1 = input("What is it called when someone hits a ball over the fence in baseball?")
+
+print(f"Today is Saturday which is also the day of the{baseballteam_1} and {baseballteam_2} game.\n They are are myfavoritet baseball teams, I love watching them and this time my dad got me and my best friend {name_1} tickets. We were so excited to go that we couldn't stop talking about the all week. Our teachers had to tell us to stop talking in glass because that's all we would talk about.  ")
+
+print(f"It was 4:30 in the after noon. The game was at 6:30 and we wanted to leave early so we can pick up {name_1} and so we have enough time to get food at the stadium. \n The game is the Indians home field in Cleveland Ohio. It's about and hour from where we live and {name_1} house is about 10 minutes from my house. Plus theres most lilkey going to traffic becuase Cleveland is very busy in the after noon.")
+
+print(f"We just got to {name_1} house to pick him up and we ran out to the car so fast. \n As soon as he got into he had a huge smile on his face, he was just as excited as I was. It was going to be an hour till were there so while we where in the car my parents asked us if we wanted to watch a movie to pass time and we both said yes. We both then agreed to watch Cars, we both liked that movie.")
+
+print(f"An hour pasted and my dad yelled WE'RE hERE! We both got so excited and unbuckled our seatbelts so fast and jumped out of the car.\n My dad parked in the parking garage right near the field so it wasn't a very far walk which was very nice. We then walked to the entrance where we had to our tickets scaned to be able to get in. We got in and the stadium was filled with people so I had to make sure me and {name_1} stayed near my parents so we don't get lost.")
+
+print(f"We walked around to find a place to eat that we liked and while looking we saw the {baseballteam_1} {maskcots_1} I and {name_1} went in to give him a hug and picture. My mom wanted a picture but I didn't mind it. Finally we found a place we all liked. Mom and dad shared a footlong sub together, {name_1} got a burger and fries, and what I got was {favoritefood}. Once we all got our food we went to go and find our seats to sit down. Good thing we got the food before the game started so we don't miss anything.")
+
+print(f"But right before we found our seats all the way in the back of the stadium me and {name_1} saw this big {color_1} bouncy slide. Our jaws dropped we {verb_1} over there as fast as we could and went down the slide at least 6 times. It was so fun but it was time to go sit down. We got back just in time before the game started. We were siting right behind centerfield. I was hoping that someone was going to hit a {batting_1} to me so I can catch it and bring it home.")
+
+print("It was the seventh inning they were both tied up i wans't sure who was going to win. But my favorite player came up to bat number 23 he gone 0-2 right now. First two pitches were strikes. The another two ball pitched both were balls. 2-2 was the count then the game wasn't tied anymore. He hits one far centerfield and came right to me over the fence a {batting_1} {baseballteam_1} WON! me and {name_1} were so happy cheering so loud just over all having a great moment. Soon came to an end when we had to leave and thats when my parents asked is we wanted to get ice cream. {icecreamflavor_1} was my favorite and immediately said yes. Then we left got our ice cream, took {name_1} home and had a reat night. THE END!") 
